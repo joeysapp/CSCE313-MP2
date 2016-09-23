13384af783ac5d233a35aabc777f17c28f2ad309
Joseph Sapp <sapp.joey@tamu.edu>
2016-09-23 15:15:33 -0500

    uhhh

--- testfile.c
+++ testfile.c
@@ -3,27 +3,14 @@
 #include "my_allocator.h"
 
 int main(int argc, char** argv){
-	init_allocator(16, 256);
 
-	//Addr add = my_malloc(128);
+	init_allocator(16, 1024);
 	
-	printf("\n@\t@\t@\t@\t@\t INSERTING 16 \t@\t@\t@\t@\t@\t\n");
-	my_malloc(16);
-
-	printf("\n@\t@\t@\t@\t@\t LIST CHECK \t@\t@\t@\t@\t@\t\n");
+//	my_malloc(16);
+//	my_malloc(16);
+//	my_malloc(128);
 	free_list_check();
-	
-	printf("\n@\t@\t@\t@\t@\t INSERTING 16 \t@\t@\t@\t@\t@\t\n");
-	my_malloc(16);
 
-	//printf("omg: %p, %i", add, (struct Node*)add);
-
-	printf("\n");
-	//my_malloc(12);
-
-	free_list_check();
-	
 	release_allocator();
 
-	//free_list_check();
 }
